07c20e6e41fc0148f2b21059b66e8759f6e33f7a
setayeshesfandiar <setayeshesf30@gmail.com>
2026-01-03 18:49:33 +0330

    Add custom exceptions: EmptyQueueException, PermissionDeniedException, UserNotFoundException, WrongPasswordException

---
+++ src/exception/EmptyQueueException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class EmptyQueueException extends Exception {
+    public EmptyQueueException(String message) {
+        super(message);
+    }
+}
---
+++ src/exception/PermissionDeniedException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class PermissionDeniedException extends Exception {
+    public PermissionDeniedException(String message) {
+        super(message);
+    }
+}
